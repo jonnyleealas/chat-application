b4a384c8abc20f7a8cbd9e9f4abbb678a66fb949
jonathon lee <jonnyleealas@gmail.com>
2020-10-09 20:05:10 -0700

    fixedenterbutton

--- public/chat.js
+++ public/chat.js
@@ -2,7 +2,7 @@
 // Make connection with html script tag
 
 let socket = io();
-
+var $window = $(window);
 // Query DOM
 let message = document.getElementById('message');
 let handle = document.getElementById('handle');
@@ -12,21 +12,42 @@ let feedback = document.getElementById('feedback');
 // Emit event when clicking send
 // this sends data to server thats listening
 button.addEventListener('click', ()=>{
+  sendMessage();
+});
+
+// The 'Enter' event handler
+$window.keydown(function (event) {
+  if (event.which === 13) {
+    sendMessage();
+  }
+});
+
+function sendMessage() {
+  if (message.value === '') {
+    return alert('Add Some text');
+
+  }
+
+  if (handle.value === '') {
+    return alert('Add a handle');
+
+  }
   socket.emit('chat', {
     message: message.value,
     handle: handle.value
-
   });
-  message.value = "";
-});
+  message.value = '';
+}
+
 message.addEventListener('keypress', ()=>{
   socket.emit('typing', handle.value);
 });
 // listen for events from server
 
 socket.on('chat', (data)=>{
-  feedback.innerHTML = "";
+  feedback.innerHTML = '';
   output.innerHTML += '<p><strong>'+ data.handle +':</strong>'+ data.message + '</p>';
+
 });
 
 socket.on('typing', (data)=>{
