b0fd79df419a11fe412cd78b2d7437be1ddad331
jonathon lee <jonnyleealas@gmail.com>
2020-10-09 19:25:26 -0700

    process

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 'use strict';
 // Make connection with html script tag
 
-let socket = io.connect('http://localhost:4000');
+let socket = io.connect(`https://chatter-ball-z.herokuapp.com/${process.env.PORT}`);
 
 // Query DOM
 let message = document.getElementById('message');
